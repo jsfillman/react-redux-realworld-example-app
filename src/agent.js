633aef9a05cf5ef09e694a4220aba6c0c5d50351
Jason S Fillman <github@jasonfillman.com>
2020-02-20 15:25:32 -0800

    Updating API_ROOT to 127.0.0.1

--- src/agent.js
+++ src/agent.js
@@ -4,7 +4,7 @@ import _superagent from 'superagent';
 const superagent = superagentPromise(_superagent, global.Promise);
 
 // const API_ROOT = 'https://conduit.productionready.io/api';
-const API_ROOT = 'http://localhost:5000/api';
+const API_ROOT = 'http://127.0.0.1:5000/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
